57c2b03057c771c80f80bda2bd2a5041eaac818c
”Defreezz” <yatsevich-artsiom@yandex.by>
2021-10-24 19:12:05 +0300

    n2

---
+++ script.js
@@ -0,0 +1,9 @@
+function alertFromInput() {
+    let inputSearch = document.getElementById('inputSearch').value;
+    if (inputSearch === 'google') {
+        alert('яндекс круче, но это не точно')
+    } else {
+        alert(inputAreaValue);
+    }
+
+}
