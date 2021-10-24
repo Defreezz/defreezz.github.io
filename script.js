b3da5e6718e26ec38ab74efdc79b9136538e1e25
”Defreezz” <yatsevich-artsiom@yandex.by>
2021-10-24 20:43:29 +0300

    n3

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+
 function alertFromInput() {
-    let inputSearch = document.getElementById('inputSearch').value;
-    if (inputSearch === 'google') {
-        alert('яндекс круче, но это не точно')
+    let inputSearchArea = document.getElementById('inputSearch').value;
+    if (inputSearchArea === 'google') {
+        alert('Яндекс круче, но это не точно')
     } else {
-        alert(inputAreaValue);
+        alert(inputSearchArea);
     }
 
 }
+document.getElementById('inputButt').addEventListener('click', alertFromInput);
