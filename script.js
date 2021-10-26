696119a6434be81984254d419227da134ed0e690
”Defreezz” <yatsevich-artsiom@yandex.by>
2021-10-26 12:58:05 +0300

    fin

--- script.js
+++ script.js
@@ -37,11 +37,70 @@ function superSum() {
 }
 document.getElementById('inputButtThree').addEventListener('click', superSum);
 
-//7
-function findMax() {
-    let nums = document.getElementById('iA').value;
-    numArray = nums.split(',');
-    alert(numArray)
+
+//7,9,10
+findMax = () => {
+    let getNum = document.getElementById('inputArray').value;
+    let newNumArray = getNum.split(',');
+    let convertToNum = newNumArray.map(function(item) {
+        return +item;
+    });
+    let bValue = convertToNum[0];
+    for (let i = 0, len = convertToNum.length; i < len; i++) {
+            if (convertToNum[i] > bValue) {
+                bValue = convertToNum[i];
+            }
+    }
+    alert(bValue);
+};
+
+showResultFindMax = () => {
+    setTimeout(function () {
+        findMax();
+    }, 3 * 1000);
+
+};
+
+findMin = () => {
+    let getNum = document.getElementById('inputArray').value;
+    let newNumArray = getNum.split(',');
+    let convertToNum = newNumArray.map(function(item) {
+        return +item;
+    });
+    let mValue = convertToNum[0];
+    for (let i = 0, len = convertToNum.length; i < len; i++) {
+            if (convertToNum[i] < mValue) {
+                mValue = convertToNum[i];
+            }
+    }
+    alert(mValue);
+};
+
+showResultFindMin = () => {
+    setTimeout(function () {
+        findMin();
+    }, 3 * 1000);
+
+};
+document.getElementById('iB').addEventListener('click', showResultFindMax);
+document.getElementById('iC').addEventListener('click', showResultFindMin);
+
+
+//8
+let divA = document.createElement('input');
+let divB = document.createElement('input');
+
+function revers() {
+    let a = document.getElementById('inA').value;
+    let b = document.getElementById('inB').value;
+    let c = b;
+    b = a;
+    a = c;
+    divA = outputArea_A.cloneNode(true);
+    divA.querySelector('p').innerHTML = a;
+    outputArea_A.after(divA);
+    divB = outputArea_B.cloneNode(true);
+    divB.querySelector('p').innerHTML = b;
+    outputArea_B.after(divB);
 }
-
-
+document.getElementById('inC').addEventListener('click', revers)
